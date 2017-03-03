a57d8244cac606280177aaeb3d04ed1dae1b1c71
Sebastian Sokolowski <sebo366@gmail.com>
2017-03-04 00:59:06 +0100

    Implement downloading all categries from server at start and save to database.

--- app/src/main/java/com/sebastian/sokolowski/auctionhunter/main/MainPresenter.java
+++ app/src/main/java/com/sebastian/sokolowski/auctionhunter/main/MainPresenter.java
@@ -3,13 +3,18 @@ package com.sebastian.sokolowski.auctionhunter.main;
 import com.alexgilleran.icesoap.exception.SOAPException;
 import com.alexgilleran.icesoap.observer.SOAPObserver;
 import com.alexgilleran.icesoap.request.Request;
-import com.sebastian.sokolowski.auctionhunter.database.entity.Target;
+import com.sebastian.sokolowski.auctionhunter.database.models.Cat;
+import com.sebastian.sokolowski.auctionhunter.database.models.Target;
 import com.sebastian.sokolowski.auctionhunter.soap.RequestManager;
 import com.sebastian.sokolowski.auctionhunter.soap.envelopes.DoGetCatsDataLimitEnvelope;
 import com.sebastian.sokolowski.auctionhunter.soap.fault.AllegroSOAPFault;
 import com.sebastian.sokolowski.auctionhunter.soap.request.SortOrderEnum;
 import com.sebastian.sokolowski.auctionhunter.soap.request.SortTypeEnum;
+import com.sebastian.sokolowski.auctionhunter.soap.response.doGetCatsDataCountResponse.DoGetCatsDataCountResponse;
+import com.sebastian.sokolowski.auctionhunter.soap.response.doGetCatsDataLimitResponse.CatInfoType;
 import com.sebastian.sokolowski.auctionhunter.soap.response.doGetCatsDataLimitResponse.DoGetCatsDataLimitResponse;
+
+import io.realm.Realm;
 
 /**
  * Created by Sebastain Sokołowski on 21.02.17.
@@ -18,36 +23,84 @@ import com.sebastian.sokolowski.auctionhunter.soap.response.doGetCatsDataLimitRe
 public class MainPresenter implements MainContract.Presenter {
 
     private final MainContract.View mView;
-    private final RequestManager requestManager;
+    private final RequestManager mRequestManager;
+    private Realm mRealm = Realm.getDefaultInstance();
 
     public MainPresenter(MainContract.View view) {
         mView = view;
-        requestManager = new RequestManager();
+        mRequestManager = new RequestManager();
     }
 
     @Override
     public void start() {
-        downloadCats();
-
+        downloadCatsCount();
     }
 
-    private void downloadCats() {
-        int i = 0;
-        int max = 10;
-        mView.showProgressDialog(max);
-        for (; i != max; i++) {
-            DoGetCatsDataLimitEnvelope doGetCatsDataLimitEnvelope = new DoGetCatsDataLimitEnvelope();
-            doGetCatsDataLimitEnvelope.setOffset(max);
-            doGetCatsDataLimitEnvelope.setPackageElement(i);
+    private void downloadCatsCount() {
+        mRequestManager.doGetCatsDataCount(new SOAPObserver<DoGetCatsDataCountResponse, AllegroSOAPFault>() {
+            @Override
+            public void onCompletion(Request<DoGetCatsDataCountResponse, AllegroSOAPFault> request) {
+                if (request.getResult() == null || request.getResult().getCatsCount() == null) {
+                    return;
+                }
 
-            requestManager.doGetCatsDataLimit(doGetCatsDataLimitEnvelope, new SOAPObserver<DoGetCatsDataLimitResponse, AllegroSOAPFault>() {
+                downloadCats(request.getResult().getCatsCount());
+            }
+
+            @Override
+            public void onException(Request<DoGetCatsDataCountResponse, AllegroSOAPFault> request, SOAPException e) {
+                if (request.getSOAPFault().getFaultString() != null) {
+                    mView.showErrorProgressDialog(request.getSOAPFault().getFaultString());
+                }
+            }
+        });
+    }
+
+    private void downloadCats(int count) {
+        //remove all old data
+        mRealm.beginTransaction();
+        mRealm.delete(Cat.class);
+        mRealm.commitTransaction();
+
+        //set progress bar max
+        int packageElement = 2500;
+        int offset = (int) Math.ceil(count / (double) packageElement);
+        mView.showProgressDialog(offset);
+        int i = 0;
+
+        for (; i != offset; i++) {
+            DoGetCatsDataLimitEnvelope doGetCatsDataLimitEnvelope = new DoGetCatsDataLimitEnvelope();
+            doGetCatsDataLimitEnvelope.setOffset(i);
+            doGetCatsDataLimitEnvelope.setPackageElement(offset);
+
+            mRequestManager.doGetCatsDataLimit(doGetCatsDataLimitEnvelope, new SOAPObserver<DoGetCatsDataLimitResponse, AllegroSOAPFault>() {
                 @Override
                 public void onCompletion(Request<DoGetCatsDataLimitResponse, AllegroSOAPFault> request) {
+                    if (request.getResult() == null || request.getResult().getCatInfoTypeList() == null) {
+                        return;
+                    }
+
+                    mRealm.beginTransaction();
+
+                    for (CatInfoType catInfoType :
+                            request.getResult().getCatInfoTypeList()
+                            ) {
+                        Cat cat = new Cat();
+                        cat.setCatId(catInfoType.getCatId());
+                        cat.setCatName(catInfoType.getCatName());
+                        cat.setCatParent(catInfoType.getCatParent());
+                        cat.setCatPosition(catInfoType.getCatPosition());
+                        cat.setCatIsProductCatalogueEnabled(catInfoType.getCatIsProductCatalogueEnabled());
+
+                        mRealm.copyToRealm(cat);
+                    }
                     mView.incrementProgressDialog();
+                    mRealm.commitTransaction();
                 }
 
                 @Override
                 public void onException(Request<DoGetCatsDataLimitResponse, AllegroSOAPFault> request, SOAPException e) {
+                    mRealm.cancelTransaction();
                     if (request.getSOAPFault().getFaultString() != null) {
                         mView.showErrorProgressDialog(request.getSOAPFault().getFaultString());
                     }
