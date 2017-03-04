f79abb9c5543f304290b680996d5aaccc5f26135
Sebastian Sokolowski <sebo366@gmail.com>
2017-03-04 21:36:11 +0100

    Change new target view. Add some new strings and add up navigation to activity.

--- app/src/main/java/com/sebastian/sokolowski/auctionhunter/newTarget/NewTargetActivity.java
+++ app/src/main/java/com/sebastian/sokolowski/auctionhunter/newTarget/NewTargetActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.sebastian.sokolowski.auctionhunter.R;
 import com.sebastian.sokolowski.auctionhunter.database.models.Target;
@@ -30,28 +31,6 @@ public class NewTargetActivity extends AppCompatActivity implements NewTargetCon
     @Override
     protected void onPause() {
         super.onPause();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_activity, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -82,5 +61,11 @@ public class NewTargetActivity extends AppCompatActivity implements NewTargetCon
     @Override
     public void showErrorDialog() {
 
+    }
+
+    public void save(View view) {
+    }
+
+    public void add_category(View view) {
     }
 }
