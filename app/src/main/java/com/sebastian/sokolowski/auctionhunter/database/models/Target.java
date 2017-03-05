5375fa0967248d31ca1b97141867fd63317331e9
Sebastian Sokolowski <sebo366@gmail.com>
2017-03-05 19:24:48 +0100

    Implement add target item methods at Target.

--- app/src/main/java/com/sebastian/sokolowski/auctionhunter/database/models/Target.java
+++ app/src/main/java/com/sebastian/sokolowski/auctionhunter/database/models/Target.java
@@ -1,23 +1,45 @@
 package com.sebastian.sokolowski.auctionhunter.database.models;
 
-import java.util.ArrayList;
+import com.sebastian.sokolowski.auctionhunter.soap.request.FilterOptionsType;
+
 import java.util.List;
+
+import io.realm.RealmList;
+import io.realm.RealmObject;
 
 /**
  * Created by Sebastain Sokołowski on 23.02.17.
  */
 
-public class Target {
+public class Target extends RealmObject {
     private String drawerName;
-    private Integer countAll;
-    private List<TargetItem> targetItemList = new ArrayList<>();
+    private RealmList<TargetItem> allItems = new RealmList<>();
+    private RealmList<TargetItem> newItems = new RealmList<>();
+    private RealmList<TargetItem> favoriteItems = new RealmList<>();
+    private RealmList<FilterOptionsType> filterOptionsTypes = new RealmList<>();
 
-    public List<TargetItem> getTargetItemList() {
-        return targetItemList;
+    public RealmList<TargetItem> getAllItems() {
+        return allItems;
     }
 
-    public void setTargetItemList(List<TargetItem> targetItemList) {
-        this.targetItemList = targetItemList;
+    public void setAllItems(RealmList<TargetItem> allItems) {
+        this.allItems = allItems;
+    }
+
+    public RealmList<TargetItem> getNewItems() {
+        return newItems;
+    }
+
+    public void setNewItems(RealmList<TargetItem> newItems) {
+        this.newItems = newItems;
+    }
+
+    public RealmList<TargetItem> getFavoriteItems() {
+        return favoriteItems;
+    }
+
+    public void setFavoriteItems(RealmList<TargetItem> favoriteItems) {
+        this.favoriteItems = favoriteItems;
     }
 
     public String getDrawerName() {
@@ -29,14 +51,50 @@ public class Target {
     }
 
     public Integer getCountAll() {
-        return countAll;
+        return allItems.size();
     }
 
-    public void setCountAll(Integer countAll) {
-        this.countAll = countAll;
+    public Integer getCountNew() {
+        return newItems.size();
     }
 
-    public Integer getCountNow() {
-        return targetItemList.size();
+    public Integer getCountFavorites() {
+        return favoriteItems.size();
+    }
+
+    public RealmList<FilterOptionsType> getFilterOptionsTypes() {
+        return filterOptionsTypes;
+    }
+
+    public void addFilterOptionsType(FilterOptionsType filterOptionsType) {
+        this.filterOptionsTypes.add(filterOptionsType);
+    }
+
+    public void addFilterOptionsTypes(List<FilterOptionsType> filterOptionsTypes) {
+        this.filterOptionsTypes.addAll(filterOptionsTypes);
+    }
+
+    public void addTargetItemToAllItems(TargetItem targetItem) {
+        this.allItems.add(targetItem);
+    }
+
+    public void addTargetItemsToAllItems(List<TargetItem> targetItems) {
+        this.allItems.addAll(targetItems);
+    }
+
+    public void addTargetItemToNewItems(TargetItem targetItem) {
+        this.newItems.add(targetItem);
+    }
+
+    public void addTargetItemsToNewItems(List<TargetItem> targetItems) {
+        this.newItems.addAll(targetItems);
+    }
+
+    public void addTargetItemToFavoriteItems(TargetItem targetItem) {
+        this.favoriteItems.add(targetItem);
+    }
+
+    public void addTargetItemsToFavoriteItems(List<TargetItem> targetItems) {
+        this.favoriteItems.addAll(targetItems);
     }
 }
