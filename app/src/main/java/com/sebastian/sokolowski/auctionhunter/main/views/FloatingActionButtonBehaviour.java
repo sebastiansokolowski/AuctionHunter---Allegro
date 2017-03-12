32c15cc318b70cf2ceadbc25a237364ccb0727d0
Sebastian Sokolowski <sebo366@gmail.com>
2017-03-12 01:24:36 +0100

    Implement show and hide FloatingActionButton on scroll RecycleView.

---
+++ app/src/main/java/com/sebastian/sokolowski/auctionhunter/main/views/FloatingActionButtonBehaviour.java
@@ -0,0 +1,43 @@
+package com.sebastian.sokolowski.auctionhunter.main.views;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Sebastian Sokołowski on 11.03.17.
+ */
+
+public class FloatingActionButtonBehaviour extends FloatingActionButton.Behavior {
+
+    public FloatingActionButtonBehaviour(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onStartNestedScroll(final CoordinatorLayout coordinatorLayout, final FloatingActionButton child,
+                                       final View directTargetChild, final View target, final int nestedScrollAxes) {
+        return true;
+    }
+
+    @Override
+    public void onNestedScroll(final CoordinatorLayout coordinatorLayout,
+                               final FloatingActionButton child,
+                               final View target, final int dxConsumed, final int dyConsumed,
+                               final int dxUnconsumed, final int dyUnconsumed) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed,dxUnconsumed, dyUnconsumed);
+        if (dyConsumed > 0 && child.getVisibility() == View.VISIBLE) {
+            child.hide(new FloatingActionButton.OnVisibilityChangedListener() {
+                @Override
+                public void onHidden(FloatingActionButton fab) {
+                    super.onShown(fab);
+                    fab.setVisibility(View.INVISIBLE);
+                }
+            });
+        } else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
+            child.show();
+        }
+    }
+}
