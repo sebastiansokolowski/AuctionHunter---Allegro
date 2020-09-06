c17f23e48bbebd9709464aa1bdcf7c3d65878c5d
Sebastian Sokołowski <sebo366@gmail.com>
2020-09-06 18:18:32 +0200

    Remove downloading all categories at start.

--- app/src/main/java/com/sebastian/sokolowski/auctionhunter/main/MainPresenter.java
+++ app/src/main/java/com/sebastian/sokolowski/auctionhunter/main/MainPresenter.java
@@ -1,24 +1,18 @@
 package com.sebastian.sokolowski.auctionhunter.main;
 
 import android.content.Context;
-import android.content.SharedPreferences;
 
 import com.alexgilleran.icesoap.exception.SOAPException;
 import com.alexgilleran.icesoap.observer.SOAPObserver;
 import com.alexgilleran.icesoap.request.Request;
 import com.sebastian.sokolowski.auctionhunter.R;
 import com.sebastian.sokolowski.auctionhunter.database.helper.FilterHelper;
-import com.sebastian.sokolowski.auctionhunter.database.models.Cat;
 import com.sebastian.sokolowski.auctionhunter.database.models.Target;
 import com.sebastian.sokolowski.auctionhunter.database.models.TargetItem;
 import com.sebastian.sokolowski.auctionhunter.soap.RequestManager;
-import com.sebastian.sokolowski.auctionhunter.soap.envelopes.DoGetCatsDataLimitEnvelope;
 import com.sebastian.sokolowski.auctionhunter.soap.fault.AllegroSOAPFault;
 import com.sebastian.sokolowski.auctionhunter.soap.request.SortOrderEnum;
 import com.sebastian.sokolowski.auctionhunter.soap.request.SortTypeEnum;
-import com.sebastian.sokolowski.auctionhunter.soap.response.doGetCatsDataCountResponse.DoGetCatsDataCountResponse;
-import com.sebastian.sokolowski.auctionhunter.soap.response.doGetCatsDataLimitResponse.CatInfoType;
-import com.sebastian.sokolowski.auctionhunter.soap.response.doGetCatsDataLimitResponse.DoGetCatsDataLimitResponse;
 import com.sebastian.sokolowski.auctionhunter.soap.response.doGetItemsListResponse.DoGetItemsListResponse;
 
 import java.util.List;
@@ -30,29 +24,22 @@ import io.realm.Realm;
  */
 
 public class MainPresenter implements MainContract.Presenter {
+    private String TAG = MainPresenter.class.getSimpleName();
 
     private final MainContract.View mView;
     private final RequestManager mRequestManager = new RequestManager();
     private Realm mRealm = Realm.getDefaultInstance();
-    private SharedPreferences mSharedPreferences;
     private Context mContext;
 
-    //cats download
-    private int completionCatsData = 0;
     private Target mCurrentTarget;
 
     public MainPresenter(MainActivity mainActivity) {
         mView = mainActivity;
         mContext = mainActivity;
-        mSharedPreferences = mainActivity.getPreferences(Context.MODE_PRIVATE);
     }
 
     @Override
     public void start() {
-        if (!mSharedPreferences.getBoolean(mContext.getString(R.string.SHARED_PREFERENCES_DOWNLOADED_CATS), false)) {
-            downloadCatsCount();
-        }
-
         refreshDrawerAdapter();
         checkIfTargetIsSelected();
     }
@@ -69,98 +56,6 @@ public class MainPresenter implements MainContract.Presenter {
             mView.showNoTargetInfo();
         } else {
             mView.setDrawerAdapterList(targetList);
-        }
-    }
-
-    private void finishedDownloadCats() {
-        SharedPreferences.Editor editor = mSharedPreferences.edit();
-        editor.putBoolean(mContext.getString(R.string.SHARED_PREFERENCES_DOWNLOADED_CATS), true);
-        editor.commit();
-    }
-
-    private void downloadCatsCount() {
-        mRequestManager.doGetCatsDataCount(new SOAPObserver<DoGetCatsDataCountResponse, AllegroSOAPFault>() {
-            @Override
-            public void onCompletion(Request<DoGetCatsDataCountResponse, AllegroSOAPFault> request) {
-                if (request.getResult() == null || request.getResult().getCatsCount() == null) {
-                    return;
-                }
-
-                downloadCats(request.getResult().getCatsCount());
-            }
-
-            @Override
-            public void onException(Request<DoGetCatsDataCountResponse, AllegroSOAPFault> request, SOAPException e) {
-                if(request.getSOAPFault() == null || request.getSOAPFault().getFaultString() == null){
-                    mView.showErrorToast(mContext.getString(R.string.main_activity_error));
-                    return;
-                }
-                if (request.getSOAPFault().getFaultString() != null) {
-                    mView.showErrorProgressDialog(request.getSOAPFault().getFaultString());
-                }
-            }
-        });
-    }
-
-    private void downloadCats(int count) {
-        //remove all old data
-        mRealm.beginTransaction();
-        mRealm.delete(Cat.class);
-        mRealm.commitTransaction();
-
-        //set progress bar max
-        int packageElement = 2500;
-        final int maxOffset = (int) Math.ceil(count / (double) packageElement);
-        mView.showProgressDialog(maxOffset);
-        int currentOffset = 0;
-        completionCatsData = 0;
-
-        for (; currentOffset != maxOffset; currentOffset++) {
-            DoGetCatsDataLimitEnvelope doGetCatsDataLimitEnvelope = new DoGetCatsDataLimitEnvelope();
-            doGetCatsDataLimitEnvelope.setOffset(currentOffset);
-            doGetCatsDataLimitEnvelope.setPackageElement(packageElement);
-
-            mRequestManager.doGetCatsDataLimit(doGetCatsDataLimitEnvelope, new SOAPObserver<DoGetCatsDataLimitResponse, AllegroSOAPFault>() {
-                @Override
-                public void onCompletion(Request<DoGetCatsDataLimitResponse, AllegroSOAPFault> request) {
-                    if (request.getResult() == null || request.getResult().getCatInfoTypeList() == null) {
-                        return;
-                    }
-
-                    mRealm.beginTransaction();
-
-                    for (CatInfoType catInfoType :
-                            request.getResult().getCatInfoTypeList()
-                            ) {
-                        Cat cat = new Cat();
-                        cat.setCatId(catInfoType.getCatId());
-                        cat.setCatName(catInfoType.getCatName());
-                        cat.setCatParent(catInfoType.getCatParent());
-                        cat.setCatPosition(catInfoType.getCatPosition());
-                        cat.setCatIsProductCatalogueEnabled(catInfoType.getCatIsProductCatalogueEnabled());
-
-                        mRealm.copyToRealm(cat);
-                    }
-                    mView.incrementProgressDialog();
-                    mRealm.commitTransaction();
-
-                    completionCatsData++;
-                    if (completionCatsData == maxOffset) {
-                        finishedDownloadCats();
-                    }
-                }
-
-                @Override
-                public void onException(Request<DoGetCatsDataLimitResponse, AllegroSOAPFault> request, SOAPException e) {
-                    if(request.getSOAPFault() == null || request.getSOAPFault().getFaultString() == null){
-                        mView.showErrorToast(mContext.getString(R.string.main_activity_error));
-                        return;
-                    }
-                    if (request.getSOAPFault().getFaultString() != null) {
-                        mView.showErrorProgressDialog(request.getSOAPFault().getFaultString());
-                    }
-                }
-            });
         }
     }
 
@@ -253,7 +148,7 @@ public class MainPresenter implements MainContract.Presenter {
 
             @Override
             public void onException(Request<DoGetItemsListResponse, AllegroSOAPFault> request, SOAPException e) {
-                if(request.getSOAPFault() == null || request.getSOAPFault().getFaultString() == null){
+                if (request.getSOAPFault() == null || request.getSOAPFault().getFaultString() == null) {
                     mView.showErrorToast(mContext.getString(R.string.main_activity_error));
                     return;
                 }
