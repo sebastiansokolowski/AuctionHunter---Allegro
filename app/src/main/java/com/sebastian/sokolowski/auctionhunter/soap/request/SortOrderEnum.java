9468fcd17e8f0d362111d3903e609bcfb37c09e2
Sebastian Sokolowski <sebo366@gmail.com>
2020-09-22 21:55:39 +0200

    Remove soap api.

--- app/src/main/java/com/sebastian/sokolowski/auctionhunter/soap/request/SortOrderEnum.java
+++
@@ -1,9 +0,0 @@
-package com.sebastian.sokolowski.auctionhunter.soap.request;
-
-/**
- * Created by Sebastain Sokołowski on 20.02.17.
- */
-
-public enum SortOrderEnum {
-    asc, desc
-}
--- app/src/main/java/com/sebastian/sokolowski/auctionhunter/soap/request/SortTypeEnum.java
+++
@@ -1,9 +0,0 @@
-package com.sebastian.sokolowski.auctionhunter.soap.request;
-
-/**
- * Created by Sebastain Sokołowski on 20.02.17.
- */
-
-public enum SortTypeEnum {
-    endingTime, startingTime, price, priceDelivery, popularity, name, relevance
-}
