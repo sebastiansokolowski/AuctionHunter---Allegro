8f0aaf250348c6b90072d3c0be3b08f1cd6b71fa
Sebastian Sokolowski <sebo366@gmail.com>
2020-10-22 00:35:50 +0200

    Spring boot init commit.

---
+++ server/src/main/kotlin/com/sebastiansokolowski/auctionhunter/AuctionhunterApplication.kt
@@ -0,0 +1,11 @@
+package com.sebastiansokolowski.auctionhunter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AuctionhunterApplication
+
+fun main(args: Array<String>) {
+	runApplication<AuctionhunterApplication>(*args)
+}
