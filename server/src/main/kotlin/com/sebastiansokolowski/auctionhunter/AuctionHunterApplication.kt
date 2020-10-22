c0ecae148dd0642a5aee251884752ef413874e1f
Sebastian Sokolowski <sebo366@gmail.com>
2020-10-22 22:11:01 +0200

    Fix typo in class name.

--- server/src/main/kotlin/com/sebastiansokolowski/auctionhunter/AuctionHunterApplication.kt
+++ server/src/main/kotlin/com/sebastiansokolowski/auctionhunter/AuctionHunterApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class AuctionhunterApplication
+class AuctionHunterApplication
 
 fun main(args: Array<String>) {
-	runApplication<AuctionhunterApplication>(*args)
+	runApplication<AuctionHunterApplication>(*args)
 }
