ea01a3384a8deec93c6beb64672dd5aa954fb96f
Sebastian Sokolowski <sebo366@gmail.com>
2020-10-25 11:05:21 +0100

    Add seller field to ListingOffer

--- server/src/main/kotlin/com/sebastiansokolowski/auctionhunter/allegro_api/response/ListingOffer.kt
+++ server/src/main/kotlin/com/sebastiansokolowski/auctionhunter/allegro_api/response/ListingOffer.kt
@@ -3,6 +3,7 @@ package com.sebastiansokolowski.auctionhunter.allegro_api.response
 data class ListingOffer (
         val id: String,
         val name: String,
+        val seller: OfferSeller,
         val delivery: OfferDelivery,
         val images: List<OfferImages>,
         val sellingMode: OfferSellingMode,
