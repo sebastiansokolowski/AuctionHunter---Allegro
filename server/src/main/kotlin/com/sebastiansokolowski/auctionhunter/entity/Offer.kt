094bc90b733936faf30edcd2ecafdfb27a30be54
Sebastian Sokolowski <sebo366@gmail.com>
2021-03-21 09:29:52 +0100

    Add name, price and url fields to Offer entity.

--- server/src/main/kotlin/com/sebastiansokolowski/auctionhunter/entity/Offer.kt
+++ server/src/main/kotlin/com/sebastiansokolowski/auctionhunter/entity/Offer.kt
@@ -8,5 +8,23 @@ data class Offer(
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         var id: Long = 0,
-        var offer_id: String = ""
-)
+        var offer_id: String = "",
+        var name: String = "",
+        var price: Float = 0.0f,
+        var url: String = ""
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Offer
+
+        if (offer_id != other.offer_id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return offer_id.hashCode()
+    }
+}
