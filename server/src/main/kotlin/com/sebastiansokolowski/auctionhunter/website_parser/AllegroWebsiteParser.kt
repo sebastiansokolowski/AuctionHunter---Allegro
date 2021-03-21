fdb4d3c474a6597ad1cc067a014b2045cca89dc6
Sebastian Sokolowski <sebo366@gmail.com>
2021-03-21 09:36:04 +0100

    Implement allegro website parser.

---
+++ server/src/main/kotlin/com/sebastiansokolowski/auctionhunter/website_parser/AllegroWebsiteParser.kt
@@ -0,0 +1,34 @@
+package com.sebastiansokolowski.auctionhunter.website_parser
+
+import com.sebastiansokolowski.auctionhunter.entity.Offer
+import com.sebastiansokolowski.auctionhunter.model.WebsiteParserModel
+import org.jsoup.nodes.Document
+
+class AllegroWebsiteParser: WebsiteParserModel.WebsiteParserEngine {
+
+    override fun websiteBaseUrl(): String = "allegro.pl"
+
+    override fun parseOffers(doc: Document): MutableList<Offer> {
+        val offers = mutableListOf<Offer>()
+
+        val elements = doc.select("article").filterNot { it.attributes().hasKey("data-analytics-view-label") }
+        elements.forEach { element ->
+            val offerId = element.attr("data-analytics-view-value")
+            val name = element.select("a._w7z6o").text()
+            val price = element.select("span._1svub").text()
+                    .replace(",", ".")
+                    .split(" ")[0].toFloat()
+            val offerUrl = element.select("a._w7z6o").attr("href")
+
+            val offer = Offer(
+                    offer_id = offerId,
+                    name = name,
+                    price = price,
+                    url = offerUrl
+            )
+            offers.add(offer)
+        }
+
+        return offers
+    }
+}
